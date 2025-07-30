a70556e265f9959ca5a769eb9b022675456af294
santanu-atta03 <santanuatta87@gmail.com>
2025-07-30 20:47:36 +0530

    for

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-redux']
+    include: ['react-redux'],
+    include: ['react-hot-toast']
   }
 });
